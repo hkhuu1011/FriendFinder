c3eeb2098f123267485dbb683ba42a361205a29a
Han Khuu <h.khuu01@gmail.com>
2017-04-30 12:45:20 -0400

    updated

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # FriendFinder
+
+### Find a friend by filling out this survey!!!
+
+- Start by cloning this repository and `npm install` the dependencies.
+
+- Make sure your localhost is set to `localhost:3009/` and start running the application by typing in `nodemon server.js` in the terminal.
+
+- Start the survey!
+
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,42 +12,80 @@ module.exports = function(app) {
 
 	// Add Post route for api
 	app.post("/api/friends", function(req, res) {
-		friendsData.push(req.body);
-		res.json(true);
-	});
+
+	// function to convert string to integer
+	function convertScores(array) {
+		var convertedArray = [];
+		for (var i = 0; i < array.length; i++) {
+			var scoreInteger = parseInt(array[i]);
+			convertedArray.push(scoreInteger);
+			}
+			return convertedArray;
+		}
+
+		var totalDifference = 0;
+		var total = 100;
+		var scoreDiff = 0;
+		var friendMatch = [];
+
+		// Adding newFriend data into JSON
+		var newFriend = (req.body)
+		// friendsData.push(newFriend);
+		console.log(newFriend);
+
+		// Converting newFriendScore string in to integer
+		var newFriendScore = convertScores(req.body.scores);
+		console.log("newFriendScore converted: ", newFriendScore);
+
+		if (friendsData.length > 0) {
+			console.log("friends data length: ", friendsData.length);
+			// Loop through each friend from data and convert string to integers
+			for (var i = 0; i < friendsData.length; i++) {
+				var friendsScoreConverted = convertScores(friendsData[i].scores);
+				console.log("Names: ", friendsData[i].name);
+				console.log("friends data converted: ", friendsScoreConverted);
+
+				// Loop through each friend's index
+				for(var j = 0; j < friendsScoreConverted.length; j++) {
+					console.log("newFriendScore[j]: ", newFriendScore[j]);
+					console.log("friendsScoreConverted[j]: ", friendsScoreConverted[j]);
+
+					// Calculate the difference between newFriend and friendsData
+					scoreDiff = Math.abs(newFriendScore[j] - friendsScoreConverted[j]);
+					console.log("index score diff: ", scoreDiff);
+					totalDifference += scoreDiff;
+					console.log("total difference: ", totalDifference);
+				} // End loop, friendsScoreConverted.length 
+
+				// If totalDifference between newFriend and friendsData score is less, then it is best matched
+				if (totalDifference < total) {
+					total = totalDifference;
+					friendMatch = friendsData[i];
+		
+				} // End if statement
+				
+					console.log("total: ", total);
+					console.log("Difference: ", totalDifference);
+					console.log("friendMatch: ", friendMatch);
+					console.log("friendsData[i].name: ", friendsData[i].name);
+					
+			} // End loop, friendsData.length
+
+			console.log("Here's your match: " + friendMatch.name);
+			console.log("Image: " + friendMatch.photo)
+			res.json(friendMatch);
+			// Add newFriend to database
+			friendsData.push(req.body);
+			res.json(true);
+
+		} // End if statement
 
 
-
-
-
-	// 	var newFriend = req.body
-	// 	// console.log(newFriend);
-	// 	friendsData.push(req.body);
-
-	// 	var newFriendScore = [];
-	// 	for (var i = 0; i < newFriend.score.length; i++) {
-	// 		newFriendScore.push(newFriend.scores[i]);
-	// 	}
-
-	// 	res.json(newFriend);
-	// });
-
-	// Handle incoming survey results
-	// Handle compatibility logic
-
-
-		// var user = req.params.friendsData;
-
-		// if (user) {
-		// 	console.log(friends);
-
-		// 	for (var i = 0; i < friendsData.length; i++) {
-		// 		if (user === friendsData[i].name) {
-		// 			return res.json(friendsData[i]);
-
-		// 		} // End if statement
-		// 	} // End for loop
-		// } // End if statement
+	}); // end app.post
 
 
 }; // End .exports function
+
+
+
+
